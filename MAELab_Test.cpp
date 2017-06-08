31eb9c07ef8513d514be1974535609e7cd9d9b02
linhlevandlu <linhlevandlu@gmail.com>
2017-06-08 22:53:04 +0200

    Add files via upload

--- MAELab_Test.cpp
+++ MAELab_Test.cpp
@@ -41,50 +41,45 @@ using namespace std;
  * Output: the output image under Binary format.
  */
 
-ptr_IntMatrix holeFill(string filename, string savename)
-{
+ptr_IntMatrix holeFill(string filename, string savename) {
 	Image image(filename);
 	Matrix<double> gauKernel = getGaussianKernel(5, 1);
 	Matrix<int> result = gaussianBlur(*image.getGrayMatrix(), gauKernel);
-	ptr_IntMatrix binMatrix = binaryThreshold(&result, image.getThresholdValue(),
-		255);
+	ptr_IntMatrix binMatrix = binaryThreshold(&result,
+			image.getThresholdValue(), 255);
 	binMatrix = postProcess(binMatrix, 255);
 	saveGrayScale(savename.c_str(), binMatrix);
 	return binMatrix;
 }
 
-Matrix<int> removelegMain(string filename, string savename)
-{
+Matrix<int> removelegMain(string filename, string savename) {
 	ptr_IntMatrix binMatrix = holeFill(filename, savename);
 	Matrix<int> result = splitImage(binMatrix, 150); // 150 --> height of sub-image
 	saveGrayScale(savename.c_str(), &result);
 	return result;
 }
 
-void getProjections(string filename, string savename)
-{
+void getProjections(string filename, string savename) {
 	Matrix<int> binMatrix = removelegMain(filename, savename);
 	ptr_IntMatrix hProjection = new Matrix<int>(binMatrix.getRows(),
-		binMatrix.getCols(), 255);
+			binMatrix.getCols(), 255);
 	ptr_IntMatrix vProjection(hProjection);
 	binProjection(&binMatrix, hProjection, vProjection);
 	analysisHistogram(hProjection, 0, 20);
 	saveGrayScale(savename.c_str(), hProjection);
 }
 
-void colorThreshold(string filename, string savename)
-{
+void colorThreshold(string filename, string savename) {
 	Image matImage(filename);
 	ptr_RGBMatrix histogram = matImage.getRGBHistogram();
 	double totalPixels = matImage.getGrayMatrix()->getRows()
-		* matImage.getGrayMatrix()->getCols();
+			* matImage.getGrayMatrix()->getCols();
 	ptr_RGBMatrix result = colorThreshold(matImage.getRGBMatrix(), histogram);
 	saveRGB(savename.c_str(), result);
 }
 
 void extractLandmarkPatch(string image_file, string landmark_file, int width,
-	int height, string save_folder)
-{
+		int height, string save_folder) {
 	Image matImage(image_file);
 	matImage.readManualLandmarks(landmark_file);
 	vector<Point> landmarks = matImage.getListOfManualLandmarks();
@@ -94,11 +89,10 @@ void extractLandmarkPatch(string image_file, string landmark_file, int width,
 
 	RGB color;
 	color.R = color.G = color.B = 0;
-	for (int i = 0; i < landmarks.size(); i++)
-	{
+	for (int i = 0; i < landmarks.size(); i++) {
 		Point pi = landmarks.at(i);
-		Matrix<int> patch = matImage.getGrayMatrix()->extractPatch(width, height,
-			pi.getY(), pi.getX(), 0);
+		Matrix<int> patch = matImage.getGrayMatrix()->extractPatch(width,
+				height, pi.getY(), pi.getX(), 0);
 		std::stringstream ssname;
 		ssname << sname;
 		ssname << "_p" << i << ".jpg";
@@ -107,17 +101,46 @@ void extractLandmarkPatch(string image_file, string landmark_file, int width,
 	}
 }
 
-void calculateSIFTDescriptor(string image_file, Point point, int patchsize)
+void calSIFT(ptr_IntMatrix imageGray, Point point, int patchsize,
+		string save_file) {
+	//ofstream inFile(savetps.c_str());
+
+	ofstream inFile(save_file.c_str());
+	//Image matImage(image_file);
+	vector<double> SIFTVector = SIFTDescriptor(imageGray, point, patchsize);
+	for (size_t i = 0; i < SIFTVector.size(); i++) {
+		if (i % 8 == 0) {
+			if(i!= 0)
+				inFile << "\n";
+			inFile << SIFTVector.at(i);
+			cout << endl << SIFTVector.at(i);
+		} else {
+			cout << "\t" << SIFTVector.at(i);
+			inFile << "\t" << SIFTVector.at(i);
+		}
+	}
+	cout << "\n";
+	inFile.close();
+}
+void calculateSIFT(string image_file, string lm_file, int patchsize, string save_folder)
 {
 	Image matImage(image_file);
-	vector<double> SIFTVector = SIFTDescriptor(matImage.getGrayMatrix(), point, patchsize);
-	for (size_t i = 0; i < SIFTVector.size(); ++i) {
-		cout<<endl<<SIFTVector.at(i);
+	matImage.readManualLandmarks(lm_file);
+	vector<Point> landmarks = matImage.getListOfManualLandmarks();
+	string name = matImage.getName();
+	size_t found2 = name.find_last_of(".");
+	string sname = name.substr(0, found2);
+	for (int i = 0; i < landmarks.size(); ++i) {
+		Point pi = landmarks.at(i);
+		std::stringstream ssname;
+		ssname << sname;
+		ssname << "_p" << i << ".txt";
+		string savename = save_folder + "/" + ssname.str();
+		calSIFT(matImage.getGrayMatrix(),pi,patchsize,savename);
 	}
 }
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
 	cout << "\n Test a function !!!" << endl;
 
 	// ================================================================ Test hole fill =================================================
@@ -125,8 +148,7 @@ int main(int argc, char* argv[])
 	string filename, savename, lm_file;
 	int width, height;
 	string save_folder;
-	if (argc == 1)
-	{
+	if (argc == 1) {
 		cout << "\nWithout parameters !!" << endl;
 		filename = "data/md028.jpg";
 		savename = "results/md028.jpg";
@@ -134,9 +156,7 @@ int main(int argc, char* argv[])
 		width = 121;
 		height = 121;
 		save_folder = "results";
-	}
-	else
-	{
+	} else {
 		cout << "\nWith parameters !!" << endl;
 		filename = argv[1];
 		//savename = argv[2];
@@ -150,6 +170,6 @@ int main(int argc, char* argv[])
 	//getProjections(filename,savename);
 	//colorThreshold(filename, savename);
 	//extractLandmarkPatch(filename, lm_file, width, height, save_folder);
-	calculateSIFTDescriptor(filename,Point(1592,1175),9);
+	calculateSIFT(filename,lm_file,9,save_folder);
 
 }
